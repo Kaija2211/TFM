5918c9926c32d42dd52a28d957d6860bfc9454ef
Kaija2211 <thomasbernards@outlook.com>
2025-12-28 16:48:19 +0000

    chore: added cube and test script
    *in borat voice* Was great success!

---
+++ FootballSimulationResearch/Assets/TestingScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          Debug.Log("Simulation project running.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
